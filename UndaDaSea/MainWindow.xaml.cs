b15b14dd10a031a1aa2d71486e6ae68476391592
Don Gordon <OpNop@users.noreply.github.com>
2020-04-28 20:32:47 -0700

    initial release
    Come Unda Da Sea

---
+++ UndaDaSea/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Gw2Sharp;
+using Gw2Sharp.Mumble;
+
+namespace UndaDaSea
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly Gw2Client client = new Gw2Client();
+        private Thread? mumbleLoopThread;
+        private bool stopRequested;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Topmost = true;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            mumbleLoopThread = new Thread(MumbleLoop);
+            mumbleLoopThread.Start();
+            underTheSea.Play();
+        }
+
+        private void underTheSea_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            underTheSea.Position = TimeSpan.Zero;
+        }
+
+        private void MumbleLoop()
+        {
+            do
+            {
+                bool shouldRun = true;
+                client.Mumble.Update();
+                if (!client.Mumble.IsAvailable)
+                    shouldRun = false;
+
+                int mapId = this.client.Mumble.MapId;
+                if (mapId == 0)
+                    shouldRun = false;
+
+                if (shouldRun)
+                {
+
+                    try
+                    {
+                        this.Dispatcher.Invoke(new Action<IGw2MumbleClient>(m =>
+                        {
+                            //Adjust volume acording to elevation
+                            double volume = 0;
+                            double Zloc = m.AvatarPosition.Y;
+                            //double maxVol = MaxVolume.Value;
+                            if (Zloc <= 0)
+                            {
+                                volume = map(Zloc, -20, 0, MaxVolume.Value, 0.05f);
+                                //wplayer.settings.volume = volume;
+                            }
+                            else
+                            {
+                                volume = map(Zloc, 0, 5, 0.05f, 0f);
+                                //wplayer.settings.volume = Math.Max(0, volume);
+                            }
+
+                            underTheSea.Volume = volume;
+                            volumeSlider.Value = volume;
+
+                        }), this.client.Mumble);
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                        // The application is likely closing
+                        break;
+                    }
+                }
+
+                Thread.Sleep(1000 / 60);
+            } while (!this.stopRequested);
+        }
+
+        private static double map(double value, double fromLow, double fromHigh, double toLow, double toHigh)
+        {
+            return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.stopRequested = true;
+        }
+    }
+}
