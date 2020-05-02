3123335e36c8413a5dcddfa009cf6f489c2fc52c
Don Gordon <OpNop@users.noreply.github.com>
2020-05-02 03:04:41 -0700

    Switch to NAudio

--- UndaDaSea/MainWindow.xaml.cs
+++ UndaDaSea/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 using System.Windows;
 using Gw2Sharp;
 using Gw2Sharp.Mumble;
+using NAudio.Wave;
 
 namespace UndaDaSea
 {
@@ -16,6 +18,11 @@ namespace UndaDaSea
         private Thread? mumbleLoopThread;
         private bool stopRequested;
 
+        //Audio Player stuff
+        private StreamMediaFoundationReader _audioFile;
+        private WaveOutEvent _outputDevice;
+        private LoopStream _loop;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -26,12 +33,14 @@ namespace UndaDaSea
         {
             mumbleLoopThread = new Thread(MumbleLoop);
             mumbleLoopThread.Start();
-            underTheSea.Play();
-        }
 
-        private void underTheSea_MediaEnded(object sender, RoutedEventArgs e)
-        {
-            underTheSea.Position = TimeSpan.Zero;
+            //Load and play
+            _audioFile = new StreamMediaFoundationReader(new MemoryStream(Properties.Resources.uts_loop4));
+            _loop = new LoopStream(_audioFile);
+            _outputDevice = new WaveOutEvent();
+            _outputDevice.Init(_loop);
+            _outputDevice.Volume = 0;
+            _outputDevice.Play();
         }
 
         private void MumbleLoop()
@@ -43,33 +52,35 @@ namespace UndaDaSea
                 if (!client.Mumble.IsAvailable)
                     shouldRun = false;
 
-                int mapId = this.client.Mumble.MapId;
+                int mapId = client.Mumble.MapId;
                 if (mapId == 0)
                     shouldRun = false;
 
                 if (shouldRun)
                 {
-
                     try
                     {
                         this.Dispatcher.Invoke(new Action<IGw2MumbleClient>(m =>
                         {
                             //Adjust volume acording to elevation
-                            double volume = 0;
-                            double Yloc = m.AvatarPosition.Y;
+                            float volume = 0;
+                            float Yloc = Convert.ToSingle(m.AvatarPosition.Y);
                             if (Yloc <= 0)
                             {
-                                volume = map(Yloc, -30, 0, MaxVolume.Value, 0.01f);
+                                volume = map(Yloc, -30, 0, Convert.ToSingle(MaxVolume.Value), 0.01f);
                             }
                             else
                             {
                                 volume = map(Yloc, 0, 3, 0.01f, 0f);
                             }
 
-                            underTheSea.Volume = volume;
+                            //Lets not get crazy here, keep it between 0 and 1
+                            volume = Clamp(volume, 0, 1);
+
+                            _outputDevice.Volume = volume;
                             volumeSlider.Value = volume;
 #if DEBUG
-                            DepthLabel.Content = $"Depth ({underTheSea.Volume})";
+                            DepthLabel.Content = $"Depth ({_outputDevice.Volume})";
 #endif
                         }), this.client.Mumble);
                     }
@@ -81,17 +92,30 @@ namespace UndaDaSea
                 }
 
                 Thread.Sleep(1000 / 60);
-            } while (!this.stopRequested);
+            } while (!stopRequested);
+
+            _outputDevice.Stop();
+            _outputDevice.Dispose();
+            _outputDevice = null;
+            _audioFile.Dispose();
+            _audioFile = null;
+            _loop.Dispose();
+            _loop = null;
+
         }
 
-        private static double map(double value, double fromLow, double fromHigh, double toLow, double toHigh)
+        private static float map(float value, float fromLow, float fromHigh, float toLow, float toHigh)
         {
             return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+        }
+        public static float Clamp(float value, float min, float max)
+        {
+            return (value < min) ? min : (value > max) ? max : value;
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            this.stopRequested = true;
+            stopRequested = true;
         }
     }
 }
